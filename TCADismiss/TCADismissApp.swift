69427f3acf31a6f82f8212f232d8b25c6a0859c2
Gábor Librecz <kuglee@gmail.com>
2024-01-12 20:19:25 +0100

    Initial Commit

---
+++ TCADismiss/TCADismissApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCADismissApp.swift
+//  TCADismiss
+//
+//  Created by Librecz Gábor on 12/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct TCADismissApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
